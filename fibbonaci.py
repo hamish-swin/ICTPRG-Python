9ec9c09a4300e15caa9f6bec181479884f71e8fb
Hamish-Swin <102687094@student.swin.edu.au>
2019-03-13 19:42:21 +1100

    Added Algorithms & Fibonnaci work

---
+++ fibbonaci.py
@@ -0,0 +1,31 @@
+
+# Task 1
+var1 = input("Please input your first number: ")
+var2 = input("Please input your second number: ")
+
+var1 = float(var1)
+var2 = float(var2)
+
+
+var4 = ((var1*var2) + (var1+var2) + (var1-var2) + (var1/var2))
+
+print ("The sum is "+str(int(var4)))
+
+if (var1 % var2 != 0):
+    print ("The outputted value has a remainder of: " + str(var1 % var2))
+
+"""
+# Fibonnaci Numbers
+arr = [0]
+
+x = z = 0
+y = 1
+
+for x in range(16):
+    a = y
+    y = z
+    z = a + y
+    arr.append(z)
+    
+print (arr)    
+"""
