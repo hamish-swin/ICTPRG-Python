b989d250325091a21f0950176c3f42100b138728
Hamish-Swin <102687094@student.swin.edu.au>
2019-03-13 17:48:12 +1100

    Initial Commit

---
+++ helloworld.py
@@ -0,0 +1,24 @@
+a = 1
+b = 2
+c = "Hey"
+
+a, b, c = 1, 2, "Hey"
+
+a = b = c = "Hey"
+
+planet = "Earth"
+print(planet)
+
+planet = "Earth"
+country = "Australia"
+print(planet, country)
+
+planet = "Earth"
+Planet = "Earth"
+Planet1 = "Earth"
+Planet_1 = "Earth"
+_planet = "Earth"
+_the_planet_with_a_circumference_of_40075_kilometers = "Earth"
+name = "Homer"
+planet = "Earth"
+print("My name is " + name + " and I live on planet " + planet)
---
+++ input.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+age = input("What is your age? ")
+sex = input("What is your gender? ")
+loc = input("Where are you from? ")
+
+print(name, "is a", age, "year old", sex, "from", loc)
+
